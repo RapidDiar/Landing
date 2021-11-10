df752dda5ab51b5b409276461b95d3d0c28ebead
anuar.kulsymanov <shol02rgb@gmail.com>
2021-11-10 13:22:47 +0600

    footer

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,15 +1,15 @@
 function Header() {
   return (
-    <div className="min-w-full  bg-gray-100">
+    <div className="min-w-full border-b-2 bg-white">
       <div className="container mx-auto flex flex-row justify-between">
         <div className="self-center">
           <p className="font-sans text-3xl">Logo</p>
         </div>
         <div className="flex flex-row space-x-4 text-2xl ">
-          <button className="border-b-4 py-7 border-gray-100 tracking-wide font-semibold hover:border-gray-300">
+          <button className="border-b-4 py-7 border-white tracking-wide font-semibold hover:border-yellow-300">
             Home
           </button>
-          <button className="border-b-4 py-7 border-gray-100 tracking-wide font-semibold hover:border-gray-300">
+          <button className="border-b-4 py-7 border-white tracking-wide font-semibold hover:border-yellow-300">
             Discover
           </button>
         </div>
